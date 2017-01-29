4618a66d53a3558e821ecdfbbb9d2e42b5c15613
Liam Bindle <lrbindle@gmail.com>
2017-01-29 00:25:12 -0600

    added pypi setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyvesc',
+  packages = ['pyvesc'],
+  version = '1.0',
+  description = 'Python implementation of the VESC communication protocol.',
+  author = 'Liam Bindle',
+  author_email = 'liambindle@gmail.com',
+  url = 'https://github.com/LiamBindle/PyVESC',
+  download_url = 'https://github.com/LiamBindle/PyVESC/tarball/1.0',
+  keywords = ['vesc', 'VESC', 'communication', 'protcol', 'packet'],
+  classifiers = [],
+)
