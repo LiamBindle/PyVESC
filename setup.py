2519c87baa9122ec3ba5dc3be182636d34fee0cc
Peter Somers <psomers3@gatech.edu>
2020-04-03 16:49:04 +0200

    switched to using crccheck

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 VERSION = '1.0.5'
 
 setup(
-  name = 'pyvesc',
-  packages = ['pyvesc', 'pyvesc.packet', 'pyvesc.messages'],
-  version = VERSION,
-  description = 'Python implementation of the VESC communication protocol.',
-  author = 'Liam Bindle',
-  author_email = 'liambindle@gmail.com',
-  url = 'https://github.com/LiamBindle/PyVESC',
-  download_url = 'https://github.com/LiamBindle/PyVESC/tarball/' + VERSION,
-  keywords = ['vesc', 'VESC', 'communication', 'protcol', 'packet'],
-  classifiers = [],
-  install_requires = ['PyCRC']
+  name='pyvesc',
+  packages=['pyvesc', 'pyvesc.packet', 'pyvesc.messages'],
+  version=VERSION,
+  description='Python implementation of the VESC communication protocol.',
+  author='Liam Bindle',
+  author_email='liambindle@gmail.com',
+  url='https://github.com/LiamBindle/PyVESC',
+  download_url='https://github.com/LiamBindle/PyVESC/tarball/' + VERSION,
+  keywords=['vesc', 'VESC', 'communication', 'protcol', 'packet'],
+  classifiers=[],
+  install_requires=['crccheck']
 )
